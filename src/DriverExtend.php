37f2bb1e41335c326e25943b9525e5057f86805f
wangtao <wangtao861024@gmail.com>
2025-10-22 18:07:08 +0800

    简化代码

--- src/DriverExtend.php
+++ src/DriverExtend.php
@@ -38,16 +38,6 @@ trait DriverExtend
         return $this->filesystem->publicUrl($path, $config);
     }
 
-    /**
-     * 自定义处理异常
-     */
-    protected function handleException(\Exception $e, string $path = ''): void
-    {
-        if(!empty($this->config['error_handler']) && is_callable($this->config['error_handler'])){
-            call_user_func($this->config['error_handler'], $e, $path);
-        }
-    }
-
     public function removePrefix(string $path): string
     {
         $prefix = $this->config['prefix'];
